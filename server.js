7ceafcad2a72e939b327789427b326522af32d57
Srivastava <achint.a.srivastava@capgemini.com>
2023-10-25 05:07:38 +0530

    no clue

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express'); 
 const app = express();
 
+const connectDb = require('./config/db');
+
+// Connecting to DB
+connectDb();
+
 app.get('/', (req,res) => res.send('API Running'));
+
+// Defining Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
